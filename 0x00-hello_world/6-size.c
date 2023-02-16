40cc0fd46c72b62dad0b87bcbabd6b1fa80e9a88
kennytobi <kennytobi@github.com>
2023-02-16 02:51:01 -0800

    alx C

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - A program that prints the various types of computers it is compiled on
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	printf("Size of char %lu bytes(s)\n", size of a (char));
+	printf("Size of int %lu bytes(s)\n", size of a (int));
+	printf("Size of short %lu bytes(s)\n", size of a (short));
+	printf("size of long %lu bytes(s)\n", size of a (long));
+	printf("size of long long %lu bytes(s)\n", size of a (long long));
+	printf("size of float %lu bytes(s)\n", size of a (float));
+	printf("size of double %lu bytes(s)\n", size of a (double));
+	printf("sie of long double %lu bytes(s)\n", size of a (long double));
+	printf("size of long int %lu bytes(s)\n", size of a (long int));
+
+	return (0);
+}
