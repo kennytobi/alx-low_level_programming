944ad1c0cb3ff1d54f49f1d3a4e79378b2665610
kennytobi <kennytobi@github.com>
2023-02-16 04:33:52 -0800

    alx C

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,11 +11,11 @@ int main(void)
 	long long int d;
 	float f;
 
-	printf("Size of char: %lu bytes(s)\n", (unsigned long)sizeof(a));
-	printf("Size of int: %lu bytes(s)\n", (unsigned long)sizeof(b));
-	printf("Size of long int: %lu bytes(s)\n", (unsigned long)sizeof(c));
-	printf("Size of long long int: %lu bytes(s)\n", (unsigned long)sizeof(d));
-	printf("Size of float: %lu bytes(s)\n", (unsigned long)sizeof(f));
+	printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of float: %lu byte(s)\n", (unsigned long)sizeof(f));
 
 	return (0);
 }
