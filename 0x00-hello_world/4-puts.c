896a1d4e41f5a9f390184bf5e0d7d14af919a279
kennytobi <kennytobi@github.com>
2023-02-16 02:07:45 -0800

    alx C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,8 @@
  * main - A Program that prints a line with put functions
  * Return: 0 (Success)
  */
-int main(void){
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
