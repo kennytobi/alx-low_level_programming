3370c99491ec4c91301d5aedbdd8f8b471bda1b9
kennytobi <kennytobi@github.com>
2023-02-16 02:23:38 -0800

    alx C

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * main - A project that prints a line with printf function
- * retun: 0 (success)
+ * return: 0 (success)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art, \n");
 
 	return (0);
 }
