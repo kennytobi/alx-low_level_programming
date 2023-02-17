d66526ec55cf6e23936a63d8eb3d37e8bcb0266a
kennytobi <kennytobi@github.com>
2023-02-17 02:44:54 -0800

    alx if

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,7 +15,7 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	printf("Last digit of %d is", n);
-		if (n%10 > 5);
+		if (n%10 > 5)
 	 printf("%d is greater than 5\n", n%10);
 		else
 		       	if (n%10 == 0)
