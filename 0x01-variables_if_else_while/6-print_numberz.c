5ece0a1fb9753fa8c783738660670df43d3741eb
kennytobi <kennytobi@github.com>
2023-02-17 06:54:26 -0800

    alx if

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,16 +7,7 @@
  */
 int main(void)
 {
-	putchar('0');
-	putchar('1');
-	putchar('2');
-	putchar('3');
-	putchar('4');
-	putchar('5');
-	putchar('6');
-	putchar('7');
-	putchar('8');
-	putchar('9');
+	putchar('123456789');
 	putchar('\n');
 	return(0);
 }
