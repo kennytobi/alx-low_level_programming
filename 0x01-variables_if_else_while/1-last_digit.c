0d88d689ed6adc30a995a388ebf7ff156e2a7965
kennytobi <kennytobi@github.com>
2023-02-17 02:35:13 -0800

    alx if

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: This program generates a random number and prints the
+ * last digit of that number along with a message describing the value
+ * of the last digits
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf("Last digit of %d is", n)
+		if (n % 10 > 5);
+	 printf("d is greater than 5\n", n % 10);
+		else if (n % 10 == 0)
+	 printf("%d and is 0\n", n % 10);
+		else
+			printf("%d is less than 6 and not 0\n", n % 10);
+	return (0);
+}
