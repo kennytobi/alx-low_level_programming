9f62318cf746d45cc0ef386e947f9bc1c0755437
kennytobi <kennytobi@github.com>
2023-02-17 01:12:07 -0800

    alx if

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - A program that prints whether the stored number in the variable is positive or negative
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if(n >= 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else
+	{
+		printf("%d is postive\n", n);
+	}
+	return (0);
+}
