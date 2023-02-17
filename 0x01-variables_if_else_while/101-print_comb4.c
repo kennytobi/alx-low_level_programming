71dcedc2e40364ebc364e70aa8770a3e8e5e67cd
kennytobi <kennytobi@github.com>
2023-02-17 12:37:54 -0800

    alx if

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,11 +7,11 @@ int main(void)
 {
 	int i,j,k;
 
-	for (i = 0; i < 8; i++)
+	for (i = 0; i < 7; i++)
 	{
-		for (j = i + 1; j < 9; j++)
+		for (j = i + 1; j < 8; j++)
 		{
-			for (k = j + 1; k < 10; k++)
+			for (k = j + 1; k < 9; k++)
 			{
 				{
 					if (i == 0 && j == 1 && k == 2)
