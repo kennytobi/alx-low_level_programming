78d840ec35fc713153d25dd2cf55c947f4084b8f
kennytobi <kennytobi@github.com>
2023-02-17 06:48:38 -0800

    alx if

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Prints all single digits numbers of base 10 starting from 0
+ * followed by a new line, using only the putchar function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	putchar('0');
+	putchar('1');
+	putchar('2');
+	putchar('3');
+	putchar('4');
+	putchar('5');
+	putchar('6');
+	putchar('7');
+	putchar('8');
+	putchar('9');
+	putchar('\n');
+	return(0);
+}
