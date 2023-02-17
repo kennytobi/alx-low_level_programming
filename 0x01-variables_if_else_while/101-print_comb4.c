4c9bcf38aed76e944a4dfbe8d935dd6736fb498d
kennytobi <kennytobi@github.com>
2023-02-17 10:31:56 -0800

    alx if

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,23 +7,28 @@ int main(void)
 {
 	int i,j,k;
 
-	for (i = 0; i < 7; i++)
+	for (i = 0; i < 8; i++)
 	{
-		for (j = i + 1; j < 8; j++)
+		for (j = i + 1; j < 9; j++)
 		{
-			for (k = j + 1; k < 9; k++)
+			for (k = j + 1; k < 10; k++)
 			{
-				putchar(i + '0');
-				putchar(j + '0');
-				putchar(k + '0');
-				if (i < 7)
 				{
-					putchar(',');
-					putchar(' ');
+					if (i == 0 && j == 1 && k == 2)
+					{
+						continue;
+					}
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(k + '0');
+					if (i < 7)
+					{
+						putchar(',');
+						putchar(' ');
+					}
 				}
 			}
 		}
+		putchar('\n');
+		return (0);
 	}
-	putchar('\n');
-	return (0);
-}
