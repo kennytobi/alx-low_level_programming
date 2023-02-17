6f33a73ab7ff87d74cba7894e46e4c1b9860d129
kennytobi <kennytobi@github.com>
2023-02-17 01:35:26 -0800

    alx if

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,21 +8,20 @@
 int main(void)
 {
 	int n;
-	srand(time(0));
+	srand(time (0));
 	n = rand() - RAND_MAX / 2;
 
-	if(n > 0)
+	if (n > 0)
 	{
 		printf("%d is positive\n", n);
 	}
-	else if(n < 0)
+	else if (n < 0)
 	{
 		printf("%d is negative\n", n);
 	}
 	else
 	{
-		printf("%d is Zero\n", n);
+		printf("%d is zero\n", n);
 	}
-	
 	return (0);
 }
