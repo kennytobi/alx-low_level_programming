60422e0036ab957faba5c5f4f1d2d6ea4a3c4172
kennytobi <kennytobi@github.com>
2023-02-17 05:10:58 -0800

    alx if

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main: Entry point
+ * Description: Prints the alphabets in lower case except q and e using
+ * only putchar
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'e' && c != 'q')
+		{
+			putchar(c);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
