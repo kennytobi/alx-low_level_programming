7a86ae5d7dbdf9a9049a5d366400783f54b6886a
kennytobi <kennytobi@github.com>
2023-02-21 05:52:36 -0800

    nested

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,10 +9,10 @@ void jack_bauer(void)
 	int i, j;
 
 	i = 0;
-	while (i < 24)
+	while (i <= 23)
 	{
 		j = 0;
-		while (j < 60)
+		while (j <= 59)
 		{
 			_putchar((i / 10) + '0');
 			_putchar((i % 10) + '0');
