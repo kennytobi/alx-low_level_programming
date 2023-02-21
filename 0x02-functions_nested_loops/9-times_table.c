dada49cc7b7d74bbb65a9441030a9bb51e89bd84
kennytobi <kennytobi@github.com>
2023-02-21 06:48:29 -0800

    nested

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,24 +4,34 @@
  */
 void times_table(void)
 {
-	int i, j, result;
+	int i, j, k;
 
-    for (i = 0; i <= 9; i++)
-    {
-        for (j = 0; j <= 9; j++)
-        {
-            result = i * j;
-            if (result > 9)
-                _putchar(result / 10 + '0');
-            else
-                _putchar(' ');
-            _putchar(result % 10 + '0');
-            if (j < 9)
-            {
-                _putchar(',');
-                _putchar(' ');
-            }
-        }
-        _putchar('\n');
-    }
+	for (i = 0; i < 10; i++)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		for (j = 1; j < 10; j++)
+		{
+			k = i * j;
+			if (k < 10)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(k + '0');
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
+			}
+			if (j < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
 }
