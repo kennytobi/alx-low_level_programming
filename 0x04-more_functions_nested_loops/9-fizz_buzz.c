5e0a48eaab297a6c0623da9835c7ff349a595184
kennytobi <kennytobi@github.com>
2023-02-23 05:51:46 -0800

    fizz bizz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,7 +5,7 @@
  * main - prints the numbers from 1 to 100, followed by a new line
  * but for multiples of three prints Fizz instead of the number
  * and for the multiples of five prints Buzz
- * Return: Always 0 (Success)
+ * Return: 0 (Success)
  */
 int main(void)
 {
@@ -27,7 +27,11 @@ int main(void)
 		}
 		else
 		{
-			printf(" %d ", i);
+			printf("%d ", i);
+		}
+		if (i % 27 == 0)
+		{
+			printf("\n");
 		}
 	}
 	printf("\n");
