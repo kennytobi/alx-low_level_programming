e03a154fce1c7e1aba5a8e2a73f78d300f59fae8
kennytobi <kennytobi@github.com>
2023-02-27 04:03:33 -0800

    functions

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * puts2 - functions that print every other character of aa string starting
+ * with a first line
+ * Return - nothing
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; ++i)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
