0a7f93ac1b41302312fd63de6f2278cbeb7b98af
kennytobi <kennytobi@github.com>
2023-02-27 01:06:01 -0800

    functions

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,8 +2,9 @@
 /**
  * reset_to_98 - updates the value of an integer to 98
  * @n: pointer to the integer to update
+ * Return: nothing
  */
-void reset_t0_98(int *n)
+void reset_to_98(int *n)
 {
 	*n = 98;
 }
