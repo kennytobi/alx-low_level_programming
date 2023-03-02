ffcfd550769311f02344c5b6a6dfe7dbac888f1c
kennytobi <kennytobi@github.com>
2023-03-02 14:20:29 -0800

    functions

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -3,17 +3,21 @@
 /**
  * print_number - print numbers chars
  * @n: integer params
+ * Return: 0
  */
 void print_number(int n)
 {
-	unsigned int n1 = n;
+	unsigned int n1;
 
+	n1 = n;
 	if (n < 0)
 	{
 		_putchar('-');
 		n1 = -n;
 	}
-	if ((n / 10) > 0)
-		print_number(n / 10);
-	_putchar((n % 10) + '0');
+	if (n1 / 10 != 0)
+	{
+		print_number(n1 / 10);
+	}
+	_putchar((n1 % 10) + '0');
 }
