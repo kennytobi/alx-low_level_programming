a24be21e92bc99835c1148503cc5dccdd3f058f1
kennytobi <kennytobi@github.com>
2023-03-02 00:28:41 -0800

    functions

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+/**
+ * string_toupper - Convert all lowercase letter in a string to uppercase.
+ * @s: the string to convert
+ * Return: A pointer to the modified string
+ */
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 97 && s[i] <= 122)
+			s[i] = s[i] - 32;
+		i++;
+	}
+	return (s);
+}
