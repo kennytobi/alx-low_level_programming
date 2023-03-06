fc4aa9185f0e3c12a78400e7e804af71015900f6
kennytobi <kennytobi@github.com>
2023-03-06 01:54:22 -0800

    pointers

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,9 +8,9 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	for (unsigned int j = 0; j < n; j++)
+	for (unsigned int i = 0; i < n; i++)
 	{
-		*(s + j) = b;
+		*(s + i) = b;
 	}
 	return (s);
 }
