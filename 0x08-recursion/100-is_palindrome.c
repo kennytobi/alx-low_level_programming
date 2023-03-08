082a0c52b29e1830a83e1f13a02db1a9ba869012
kennytobi <kennytobi@github.com>
2023-03-08 05:59:37 -0800

    recursion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -37,3 +37,16 @@ int comparator(char *s, int n1, int n2)
 	}
 	return (0);
 }
+/**
+ * _strlen_recursion - returns the length of a string.
+ * @s: string
+ * Return: the length of a string.
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
+}
