0183908cee37c9dd63796c9f3f78cf0c180f3457
kennytobi <kennytobi@github.com>
2023-03-08 05:44:39 -0800

    recursion

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+/**
+ * is_palindrome - Checks whether a string is a palindrome.
+ *
+ * @s: The string to check.
+ *
+ * Return: 1 if the string is a palindrome, 0 otherwise.
+ */
+int is_palindrome(char *s)
+{
+	int i, j;
+	int len = strlen(s);
+
+	for (i = 0, j = len - 1; i < j; i++, j--)
+	{
+		if (s[i] != s[j])
+		{
+			return (0);
+		}
+	}
+	return (1);
+}
