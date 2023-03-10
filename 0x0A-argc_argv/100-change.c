1a3a9deea3dbd2d8778ddc4b7ab3a9b0d0f1e8f9
kennytobi <kennytobi@github.com>
2023-03-10 05:54:59 -0800

    argc argv

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -9,20 +9,19 @@
  */
 int main(int argc, char *argv[])
 {
+	int cents, coins = 0;
+
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	int cents = atoi(argv[1]);
-
+	cents = atoi(argv[1]);
 	if (cents < 0)
 	{
 		printf("0\n");
 		return (0);
 	}
-	int coins = 0;
-
 	while (cents > 0)
 	{
 		if (cents >= 25)
