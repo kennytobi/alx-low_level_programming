be883184956f784d1446564e98ea8c8af53620e4
kennytobi <kennytobi@github.com>
2023-03-10 04:31:14 -0800

    argc argv

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,4 +1,8 @@
-#ifndef MAIN_H
-#define MAIN_H
-int main(int argc __attribute__((unused)), char *argv[]);
+#ifndef MAIN_H_
+#define MAIN_H_
+
+int _putchar(char c);
+int _atoi(char *s);
+int check_num(char *str);
+
 #endif
