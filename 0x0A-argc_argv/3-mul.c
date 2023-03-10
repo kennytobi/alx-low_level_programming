53ece80bc5ffa27c82f9e88d6e88b4ba7d83c909
kennytobi <kennytobi@github.com>
2023-03-10 05:02:08 -0800

    argc argv

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - program that multiplies two numbers.
+ * @argc: the number of arguments passed to the program
+ * @argv: the array of string containing the argument passed to the program
+ * Return: 0 if successful, 1 otherwise
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2, result;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	result = num1 * num2;
+	printf("%d\n", result);
+	return (0);
+}
