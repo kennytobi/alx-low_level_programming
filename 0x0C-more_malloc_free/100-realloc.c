2f5c7c3b01e1a0d5a6d299e83a9aafad6763e75a
kennytobi <kennytobi@github.com>
2023-03-16 07:49:24 -0700

    more malloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -12,6 +12,16 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	char *new_ptr;
 
+	if (new_size == old_size)
+		return (ptr);
+	if (ptr == NULL)
+	{
+		new_ptr = malloc(new_size);
+		if (new_ptr == NULL)
+			return (NULL);
+		free(ptr);
+		return (new_ptr);
+	}
 	if (new_size == 0)
 	{
 		free(ptr);
