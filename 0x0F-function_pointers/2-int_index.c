3a57bf4085825d5f5688e2086bcdd90ddc7254ce
kennytobi <kennytobi@github.com>
2023-03-22 07:00:07 -0700

    function pointers

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * int_index - function to return index of compound int
+ * @array: pointer to array of int type
+ * @size: size of array of int type
+ * @cmp: pointer to function of int type
+ * Return: return index number of comp int
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || cmp == NULL)
+	{
+		return (-1);
+	}
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+		{
+			return (i);
+		}
+	}
+	return (-1);
+}
