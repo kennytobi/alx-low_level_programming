e05e3429705a56953ad131d4561f0b66294fa2ae
kennytobi <kennytobi@github.com>
2023-03-22 04:44:07 -0700

    function pointers

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,6 +11,12 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
+	if (action == NULL)
+		return;
+	if (size <= 0)
+		return;
+	if (array == NULL)
+		return;
 	for (i = 0; i < size; i++)
 	{
 		action(array[i]);
