c8db38498508c933dbec21a958fc0e2eb3cdba6e
kennytobi <kennytobi@github.com>
2023-03-22 08:45:47 -0700

    function pointers

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,52 @@
+#include "3-calc.h"
+#include <stdio.h>
+/**
+ * op_add - add function
+ * @a: int type
+ * @b: int type
+ * Return: sum of two int
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - substract function
+ * @a: int type
+ * @b: int type
+ * Return: substract two ints
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiply function
+ * @a: int type
+ * @b: int type
+ * Return: multiply two ints
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - multiply function
+ * @a: int type
+ * @b: int type
+ * Return: multiply two ints
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - multiply function
+ * @a: int type
+ * @b: int type
+ * Return: multiply two ints
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
