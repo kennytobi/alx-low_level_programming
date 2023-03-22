2067f8967f276501e7f53cd5080ed153be96e6e9
kennytobi <kennytobi@github.com>
2023-03-22 15:22:42 -0700

    function pointer

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,33 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Description: program prints opcodes of it own main function
+ * @argc: argument counter
+ * @argv: vector argument
+ * Return: success
+ */
+int main(int argc, char *argv[])
+{
+	int num_bytes, i;
+	unsigned char *ptr = (unsigned char *) main;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	num_bytes = atoi(argv[1]);
+	if (num_bytes < 0)
+	{
+		printf("Error\n");
+		return (2);
+	}
+	for (i = 0; i < num_bytes; i++)
+	{
+		printf("%02x ", *(ptr + i));
+	}
+	printf("\n");
+	return (0);
+}
