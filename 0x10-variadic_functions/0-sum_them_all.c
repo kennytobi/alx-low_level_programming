f86f33a76ba36c0d9db19312bfb329d3ff5ec415
kennytobi <kennytobi@github.com>
2023-03-23 05:24:01 -0700

    variadic

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ * sum_them_all - sum add the paramemters
+ * @n: integer input
+ * Return: the sum
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list args;
+	int i, sum;
+
+	sum = 0;
+	if (n == 0)
+	{
+		return (0);
+	}
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(args, int);
+	}
+	va_end(args);
+	return (sum);
+}
