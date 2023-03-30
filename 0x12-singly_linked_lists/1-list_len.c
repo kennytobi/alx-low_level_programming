f3b42cb623ef32ff6616ac7e00dbac8142835348
kennytobi <kennytobi@github.com>
2023-03-30 05:45:29 -0700

    linked list

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * list_den - returns the number of elements in a linked list_t list
+ * list_len - returns the number of elements in a linked list_t list
  * @h: pointer to the head node of the linked list
  * Return: the number of nodes in the list
  */
