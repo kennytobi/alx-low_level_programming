49b817ebf8f0a0969de0934daab43abd7de49a19
kennytobi <kennytobi@github.com>
2023-03-30 07:52:31 -0700

    linked list

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,5 +1,8 @@
+bits 64
+default rel
+
 section .data
-message db 'Hello, Hoberton',0Ah ; Message to print
+message db 'Hello, Holberton',0Ah ; Message to print
 
 section .text
 global main
@@ -8,7 +11,7 @@ extern printf
 main:
 push message ; Push message address onto stack
 mov rdi, message ; set first argument to message address
-xor eax, eax ; clear eax register
+xor eax, eax ; Clear eax register
 call printf ; call printf function
 add rsp, 8 ; Remove message address from stack
 ret ; Return from main
