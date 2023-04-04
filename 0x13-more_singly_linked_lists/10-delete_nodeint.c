28b99566db8557fb5711c7b4f695017d4234d922
kennytobi <kennytobi@github.com>
2023-04-04 08:40:01 -0700

    more linked list

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -20,7 +20,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (1);
 	}
 	prev = *head;
-	for (i = 0; prev != NULL && i < index -1; i++)
+	for (i = 0; prev != NULL && i < index - 1; i++)
 		prev = prev->next;
 	if (prev == NULL || prev->next == NULL)
 		return (-1);
